594706c448f7daafba3e79b41c8831683526f25c
Tianlong Wang <tianlong.wang.13@googlemail.com>
2015-05-05 09:25:10 +0800

    init commit, groups

---
+++ client/template/groups/groups_list.js
@@ -0,0 +1,15 @@
+var groupsData = [ {
+    title: "蜀国",
+    author: "刘备",
+    url : "http://www.baidu.com/"
+  
+  },
+  {
+    title: "汉朝群雄",
+    author: "汉献帝",
+    url : "http://www.baidu.com/"
+  }
+];
+Template.groupsList.helpers({
+    groups: groupsData
+});
