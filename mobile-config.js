51d724c4ef293529cd0059da312e96939f3c9960
Tianlong Wang <tianlong.wang.13@googlemail.com>
2015-05-12 12:02:11 +0800

    mobile configuration

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  name: '群秘',
+  description: '最好的大群管家',
+  version: '0.0.1'
+});
+/*
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
+*/
